f667fa45bb85bf079c67949359b0fec1651732f0
Himanshu0894 <84079963+Himanshu0894@users.noreply.github.com>
2021-12-05 16:57:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//colors array
+let colors = ['blue','yellow','black','red','brown','orange'];
+//get button
+let button = document.getElementById('button');
+//add event listener
+button.addEventListener('click', function(){
+     var randomColor = colors[Math.floor(Math.random()* colors.length)]
+     //get container
+     let container = document.getElementById('container');
+     container.style.backgroundColor = randomColor;
+})
